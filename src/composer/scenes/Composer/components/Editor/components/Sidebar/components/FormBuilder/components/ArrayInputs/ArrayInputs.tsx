0266317b6d772761337e2e4b94811a4ec81de2c0
Nick K <studioo@mail.ru>
2019-01-05 15:26:11 +0100

    Update Editor component: change tabs to accordion

--- src/composer/scenes/Composer/components/Editor/components/Sidebar/components/FormBuilder/components/ArrayInputs/ArrayInputs.tsx
+++ src/composer/scenes/Composer/components/Editor/components/Sidebar/components/FormBuilder/components/ArrayInputs/ArrayInputs.tsx
@@ -24,20 +24,21 @@ class ArrayInputs extends React.Component<IArrayInputsProps> {
   constructor(props: IArrayInputsProps) {
     super(props);
     this.onChange = this.onChange.bind(this);
+    this.onEditTab = this.onEditTab.bind(this);
     this.mediaLibraryChange = this.mediaLibraryChange.bind(this);
     this.onChangeTab = debounce(this.onChangeTab.bind(this), 25);
-    console.log(this.props.activeTab);
   }
 
-  public onChangeTab(key: number) {
+  public onChangeTab(key: string) {
     this.props.onChange({ target: { name: 'activeTab', value: key } });
   }
 
   public onNewTab() {
+    const id = `#${Date.now()}`;
     const newData = [...this.props.data];
-    newData.push({});
+    newData.push({ id });
 
-    const newTab = newData.length - 1;
+    const newTab = id;
 
     this.props.onChange({
       target: {
@@ -54,7 +55,6 @@ class ArrayInputs extends React.Component<IArrayInputsProps> {
     let newData = [...this.props.data];
     let newTab = this.props.activeTab;
 
-    // remove tab
     if (action === 'remove') {
       newData.splice(iKey, 1);
 
@@ -66,20 +66,40 @@ class ArrayInputs extends React.Component<IArrayInputsProps> {
       }
     }
 
+    if (action === 'up') {
+      if (iKey > 0 && newData.length > 1) {
+        [newData[iKey], newData[iKey - 1]] = [newData[iKey - 1], newData[iKey]];
+      }
+    }
+
+    if (action === 'down') {
+      if (iKey < newData.length - 1 && newData.length > 1) {
+        [newData[iKey], newData[iKey + 1]] = [newData[iKey + 1], newData[iKey]];
+      }
+    }
+
     this.props.onChange({
       target: {
         name: this.props.name,
         value: newData,
       },
     });
-
-    this.onChangeTab(newTab);
   }
 
   // tslint:disable-next-line:no-any
   public onChange(key: any) {
+    let rowIndex = this.props.data.findIndex((row: ILooseObject) => row.id === this.props.activeTab);
+    
+    if (rowIndex < 0) {
+      rowIndex = 0;
+    }
+
     const newData = [...this.props.data];
-    newData[this.props.activeTab][key.target.name] = key.target.value;
+    newData[rowIndex][key.target.name] = key.target.value;
+   
+    if (!newData[rowIndex].id) {
+      newData[rowIndex].id = `#${Date.now()}`;
+    }
 
     this.props.onChange({
       target: {
@@ -90,8 +110,18 @@ class ArrayInputs extends React.Component<IArrayInputsProps> {
   }
 
   public mediaLibraryChange(media: { value: object; name: string }) {
+    let rowIndex = this.props.data.findIndex((row: ILooseObject) => row.id === this.props.activeTab);
+
+    if (rowIndex < 0) {
+      rowIndex = 0;
+    }
+
     const newData = [...this.props.data];
-    newData[this.props.activeTab][media.name] = media.value;
+    newData[rowIndex][media.name] = media.value;
+
+    if (!newData[rowIndex].id) {
+      newData[rowIndex].id = `#${Date.now()}`;
+    }
 
     this.props.onChange({
       target: {
@@ -105,76 +135,75 @@ class ArrayInputs extends React.Component<IArrayInputsProps> {
     return (
       <Section title={this.props.title}>
         {/* 
-          TODO: If there accordion=false the bugs is commig beacause activeTab just a single number
+          If there accordion=false the bugs is commig beacause activeTab just a single number
         */}
-        <Collapse accordion={true} onChange={(key: string) => this.onChangeTab(parseInt(key, 10))}>
+
+        <Collapse accordion={true} onChange={(key: string) => this.onChangeTab(key)}>
           {this.props.data &&
             this.props.data.map((dataRow: ILooseObject, index: number) => {
-              const dataRowKeys = dataRow && Object.keys(dataRow);
-              let title = '—';
-              if (Array.isArray(dataRowKeys) && dataRowKeys.length > 0) {
-                title = dataRow[dataRowKeys[0]];
+              let title = null;
+              if (this.props.items.properties) {
+                title = dataRow[Object.keys(this.props.items.properties)[0]];
               }
 
               const panelTitle = (
                 <>
-                  {title}
+                  <div onClick={e => { e.stopPropagation(); }}>
+                    {title || 'new item'}
 
-                  {/* TODO: onClick */}
-                  <div style={{position: 'absolute', top: '30%', right: '35px' }}>
-                    <Icon onClick={() => alert('UP')} type="arrow-up" style={{ marginRight: '5px' }} />
-                    <Icon onClick={() => alert('DOWN')} type="arrow-down" />
+                    <div style={{position: 'absolute', top: '30%', right: '35px' }}>
+                      <Icon onClick={() => this.onEditTab(index.toString(), 'up')} type="arrow-up" style={{ marginRight: '5px' }} />
+                      <Icon onClick={() => this.onEditTab(index.toString(), 'down')} type="arrow-down" />
+                    </div>
+
+                    <Popconfirm
+                      title="Are you sure delete this tab?"
+                      onConfirm={() => this.onEditTab(index.toString(), 'remove')}
+                      okText="Yes"
+                      cancelText="No"
+                    >
+                      <Icon
+                        type="close"
+                        theme="outlined"
+                        style={{ color: '#f5222d', position: 'absolute', top: '40%', right: '15px' }}
+                        className="anticon anticon-close ant-tabs-close-x"
+                      />
+                    </Popconfirm>
                   </div>
-
-                  <Popconfirm
-                    title="Are you sure delete this tab?"
-                    onConfirm={() => this.onEditTab(index.toString(), 'remove')}
-                    okText="Yes"
-                    cancelText="No"
-                  >
-                    <Icon
-                      type="close"
-                      theme="outlined"
-                      style={{ color: '#f5222d', position: 'absolute', top: '40%', right: '15px' }}
-                      className="anticon anticon-close ant-tabs-close-x"
-                    />
-                  </Popconfirm>
                 </>
               );
 
               return (
-                <Collapse.Panel key={index.toString()} header={panelTitle}>
+                <Collapse.Panel key={dataRow.id} header={panelTitle}>
                   {this.props.items &&
                     this.props.items.properties &&
                     Object.keys(this.props.items.properties).map((elementName: string, j: number) => {
                       const element = this.props.items.properties[elementName];
                       return (
-                        <InputRenderer
-                          key={j}
-                          id={index}
-                          name={elementName}
-                          language={this.props.language}
-                          {...element}
-                          value={
-                            this.props.data && this.props.data[index] && this.props.data[index][elementName]
-                              ? this.props.data[index][elementName]
-                              : null
-                          }
-                          onChange={this.onChange}
-                          mediaLibraryChange={this.mediaLibraryChange}
-                        />
+                        <>
+                          <InputRenderer
+                            key={`${dataRow.id}_${j}`}
+                            id={`${dataRow.id}_${j}`}
+                            name={elementName}
+                            language={this.props.language}
+                            {...element}
+                            value={dataRow[elementName]}
+                            onChange={this.onChange}
+                            mediaLibraryChange={this.mediaLibraryChange}
+                          />
+                        </>
                       );
                     })}
                 </Collapse.Panel>
               );
             })}
-            <div className={'ant-collapse-item'}>
+            <div className={'ant-collapse-item'} style={{ backgroundColor: 'white' }}>
               <a 
                 className={'ant-collapse-header'} 
                 onClick={() => this.onNewTab()} 
                 style={{ display: 'block', padding: '10px 0', textAlign: 'center', color: '#1890ff' }}
               >
-                Add new element
+                Add new item
               </a>
             </div>
         </Collapse>
