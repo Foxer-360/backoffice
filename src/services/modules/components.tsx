68ce6449c2275289d9537682d62ecf91975b9303
Filip Suchý <nevim42@gmail.com>
2019-02-06 21:02:01 +0100

    Added components per project. In Selector scene, you can edit project and there select components, which will be allowed in this project.

--- src/services/modules/components.tsx
+++ src/services/modules/components.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { components, config, LibDefinition } from './config';
+import { client, queries } from '../graphql';
 
 interface NamedInstances {
   // tslint:disable-next-line:no-any
@@ -9,6 +10,43 @@ interface NamedInstances {
 interface Name2InstanceMap {
   [name: string]: string;
 }
+
+interface Names {
+  [name: string]: string[];
+}
+
+// Helper to get project id
+const getProjectId = (): string | null => {
+  let { website } = client.cache.readQuery({ query: queries.LOCAL_SELECTED_WEBSITE });
+  const data = client.cache.readQuery({ query: queries.WEBSITE_DETAIL, variables: { id: website } }) as LooseObject;
+
+  if (!data || !data.website || !data.website.project || !data.website.project.id) {
+    return null;
+  }
+
+  const project = data.website.project.id;
+
+  return project;
+};
+
+// Get allowed components for project or null to enable all
+const getProjectComponents = (id: string): string[] | null => {
+  if (!id || id.length < 2) {
+    return null;
+  }
+
+  const { project } = client.cache.readQuery({ query: queries.GET_PROJECT, variables: { id } });
+  if (!project || !project.components || project.components.length < 2) {
+    return null;
+  }
+
+  return project.components;
+};
+
+// This will return all available components
+export const getComponentSets = () => {
+  return config.components.map((lib: LibDefinition) => lib.name);
+};
 
 class NotFound extends React.Component<{}, {}> {
 
@@ -27,7 +65,7 @@ class NotFound extends React.Component<{}, {}> {
  */
 class ComponentsModule {
 
-  private names: string[];
+  private names: Names;
 
   private name2instance: Name2InstanceMap;
 
@@ -38,7 +76,7 @@ class ComponentsModule {
    * Prepare this module by merging all components from dependencies
    */
   constructor() {
-    this.names = [];
+    this.names = {};
     this.instances = {};
     this.name2instance = {};
 
@@ -51,10 +89,7 @@ class ComponentsModule {
       types.forEach((type: string) => {
         this.name2instance[type] = lib.name;
       });
-      this.names = [
-        ...this.names,
-        ...types,
-      ];
+      this.names[lib.name] = types;
     });
   }
 
@@ -62,10 +97,40 @@ class ComponentsModule {
    *
    */
   public getAllowedTypes() {
-    return this.names;
+    const id = getProjectId();
+    let allowed = getProjectComponents(id);
+    const names = config.components.map((lib: LibDefinition) => lib.name);
+
+    // All components
+    if (!allowed || allowed.length < 1) {
+      let res = [];
+      names.forEach((n: string) => {
+        res = [
+          ...res,
+          ...this.names[n]
+        ];
+      });
+
+      return res;
+    }
+
+    let result = [];
+    names.forEach((n: string) => {
+      if (!allowed.includes(n)) {
+        return;
+      }
+
+      result = [
+        ...result,
+        ...this.names[n]
+      ];
+    });
+
+    return result;
   }
 
   public getComponent(type: string) {
+    getProjectId();
     const name = this.name2instance[type];
     const i = this.instances[name];
 
@@ -77,6 +142,7 @@ class ComponentsModule {
   }
 
   public getComponentResource(type: string) {
+    getProjectId();
     const name = this.name2instance[type];
     const i = this.instances[name];
 
@@ -88,6 +154,7 @@ class ComponentsModule {
   }
 
   public getForm(type: string) {
+    getProjectId();
     const name = this.name2instance[type];
     const i = this.instances[name];
 
